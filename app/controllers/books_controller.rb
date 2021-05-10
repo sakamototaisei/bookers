cc95db2cda0b6b4cb2743d0db6f6a090b9003e2c
sakamototaisei <taisei0403089@gmail.com>
2021-05-10 10:56:33 +0000

    added controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all.order(created_at: :desc)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to ("/book/#{book.id}")
+  end
+
+  def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
