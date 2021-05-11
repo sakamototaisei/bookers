4a9b68e3cadccc9d9404aff992118f6ad13871a6
sakamototaisei <taisei0403089@gmail.com>
2021-05-11 10:00:17 +0000

    added update delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all.order(created_at: :desc)
+    @book = Book.new
   end
 
   def show
@@ -9,17 +10,31 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
   end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to ("/book/#{book.id}")
+    redirect_to ("/books/#{book.id}")
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  
   
   private
   def book_params
